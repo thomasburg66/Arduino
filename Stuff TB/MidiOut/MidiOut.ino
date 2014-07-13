6b2edd39500917069ea04f4905797e84871aaa23
Thomas Burg <t1@thomas-burg.de>
2014-07-13 17:01:07 +0200

    Version works as expected
    Connect "Sparkfun Midi Shield" midi out to midi in of midi controller TB
    (Tascam) // watch output with Midi Ox // press of a button will play
    note depending on potentiometer A1
    
    MidiOx sample output
    
    000AD674   1  --     95    7F    45    6  G  9 Note On
    000AD675   1  --     95    7F    45    6  G  9 Note On
    000AD676   1  --     95    7F    45    6  G  9 Note On
    000AD677   1  --     95    7F    45    6  G  9 Note On
    000AD677   1  --     95    7F    45    6  G  9 Note On
    000AD678   1  --     95    7F    45    6  G  9 Note On
    000AD679   1  --     95    7F    45    6  G  9 Note On
    000AD67A   1  --     95    7F    45    6  G  9 Note On
    000AD744   1  --     95    7F    45    6  G  9 Note On
    000ADED1   1  --     95    60    45    6  C  7 Note On
    000AE2A7   1  --     95    1B    45    6  Eb 1 Note On
    000AE2A9   1  --     95    1B    45    6  Eb 1 Note On
    000AE2A9   1  --     95    1B    45    6  Eb 1 Note On
    000AE92F   1  --     95    43    45    6  G  4 Note On
    000AE9B6   1  --     95    43    45    6  G  4 Note On

--- Stuff TB/MidiOut/MidiOut.ino
+++ Stuff TB/MidiOut/MidiOut.ino
@@ -11,8 +11,7 @@
 #define STAT1  7
 #define STAT2  6
 
-
-
+#include "WProgram.h"
 void setup();
 void loop ();
 void noteOn(byte cmd, byte data1, byte data2);
@@ -41,7 +40,7 @@ void setup() {
   pinMode(BUTTON2,INPUT);
   pinMode(BUTTON3,INPUT);
 
-midi
+
   digitalWrite(BUTTON1,HIGH);
   digitalWrite(BUTTON2,HIGH);
   digitalWrite(BUTTON3,HIGH);
@@ -65,12 +64,103 @@ midi
   //Serial.println("MIDI Board");  
 }
 
+//loop: wait for serial data, and interpret the message
+void loop () {
+
+
+  /* 
+   // Button and knob test functions
+   if(analogRead(KNOB1) > 512){ digitalWrite(STAT1,HIGH); }
+   else{ digitalWrite(STAT1,LOW); }
+   if(analogRead(KNOB2) > 512){ digitalWrite(STAT2,HIGH); }
+   else{ digitalWrite(STAT2,LOW); }
+   
+   
+   if(!digitalRead(BUTTON1)){ digitalWrite(STAT1,LOW); digitalWrite(STAT2,LOW); }
+   else if(!digitalRead(BUTTON2)){ digitalWrite(STAT1,HIGH); digitalWrite(STAT2,LOW); }
+   else if(!digitalRead(BUTTON3)){ digitalWrite(STAT1,LOW); digitalWrite(STAT2,HIGH); }
+   else if(!digitalRead(BUTTON4)){ digitalWrite(STAT1,HIGH); digitalWrite(STAT2,HIGH); }
+   */
+  
+  //*************** MIDI OUT ***************//
+
+  pot = analogRead(0);
+  note = pot/8;  // convert value to value 0-127
+  if(button(BUTTON1) || button(BUTTON2) || button(BUTTON3))
+  {
+    
+    noteOn(0x95,0x3C,0x45);
+    digitalWrite(STAT2,LOW);
+    while(button(BUTTON1) || button(BUTTON2) || button(BUTTON3));
+  }
+  /*while(1);
+  else
+  {
+    noteOn(0x90,0x3C,0x00);
+    digitalWrite(STAT2,HIGH);
+  }*/
+  
+  //*************** MIDI LOOPBACK ******************//
+  /*
+  if(Serial.available() > 0)
+  {
+    byte1 = Serial.read();
+    byte2 = Serial.read();
+    byte3 = Serial.read();
+  
+    noteOn(byte1, byte2, byte3);
+    
+    //digitalWrite(STAT1,HIGH);  
+    //digitalWrite(STAT2,HIGH);
+  }*/
+  //*************** MIDI IN ***************//
+  /*
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+
+    // wait for as status-byte, channel 1, note on or off
+    if (incomingByte== 144){ // note on message starting starting
+      action=1;
+    }
+    else if (incomingByte== 128){ // note off message starting
+      action=0;
+    }
+    else if (incomingByte== 208){ // aftertouch message starting
+      //not implemented yet
+    }
+    else if (incomingByte== 160){ // polypressure message starting
+      //not implemented yet
+    }
+    else if ( (action==0)&&(note==0) ){ // if we received a "note off", we wait for which note (databyte)
+      note=incomingByte;
+      playNote(note, 0);
+      note=0;
+      velocity=0;
+      action=2;
+    }
+    else if ( (action==1)&&(note==0) ){ // if we received a "note on", we wait for the note (databyte)
+      note=incomingByte;
+    }
+    else if ( (action==1)&&(note!=0) ){ // ...and then the velocity
+      velocity=incomingByte;
+      playNote(note, velocity);
+      note=0;
+      velocity=0;
+      action=0;
+    }
+    else{
+      //nada
+    }
+  }*/
+  
+}
 
 void noteOn(byte cmd, byte data1, byte data2) {
-  Serial.print(cmd);
-  Serial.print(data1);
-  Serial.println(data2);
-}
+   Serial.print(cmd, BYTE);
+   Serial.print(data1, BYTE);
+   Serial.print(data2, BYTE);
+ }
 
 void blink(){
   digitalWrite(STAT1, HIGH);
@@ -104,17 +194,15 @@ char button(char button_num)
   return (!(digitalRead(button_num)));
 }
 
-void loop () {
+int main(void)
+{
+	init();
 
-  pot = analogRead(1);
-  note = pot/8;  // convert value to value 0-127
-  if(button(BUTTON1) || button(BUTTON2) || button(BUTTON3))
-  {
+	setup();
     
-    noteOn(0x95,0x3C,0x45);
-    digitalWrite(STAT2,LOW);
-    while(button(BUTTON1) || button(BUTTON2) || button(BUTTON3));
-  }
-
+	for (;;)
+		loop();
+        
+	return 0;
 }
 
